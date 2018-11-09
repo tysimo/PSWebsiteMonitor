16c7b95072bee82877b9f5a9e7e84091fae65352
Tyler <tsimonson@imagetrend.com>
2018-11-09 14:14:34 -0600

    Initial commit

--- PSWebsiteMonitor.psm1
+++ PSWebsiteMonitor.psm1
@@ -1,18 +1,10 @@
-Function UptimeCheck ($Environment, $Group)
+Function UptimeCheck ($Group, $Instance, $Database, $MailServer, $FromAddres)
 {
-	[xml]$xml = get-content .\environment.xml
-	$Environment = $xml.environments.environment | where {$_.id -eq $Environment}
-	
-	$Instance = $Environment."database-instance"
-	$Database = $Environment."database-name"
-	$MailServer = $Environment."mail-server"
-	$FromAddress = $Environment."mail-from-address"
-
 	$AllProtocols = [System.Net.SecurityProtocolType]'Ssl3,Tls,Tls11,Tls12'
 	[System.Net.ServicePointManager]::SecurityProtocol = $AllProtocols
 
 	$URLs = Invoke-SQLCmd -Query "select u.id as id, url, description, failcount, g.notificationlist as notificationlist, timeout, lasthealthycheck from url u inner join [group] g on u.group_id = g.id where active = 1 and name = '$Group'" -ServerInstance $Instance -Database $Database
-
+	
 	ForEach ($URL in $URLs)
 	{
 		$ID = $URL.id
@@ -32,14 +24,17 @@ Function UptimeCheck ($Environment, $Group)
 		}  
 		Catch
 		{
-			$Status = $_.Exception
-			$Status = $Status -replace "'",""
-			$Now = Get-Date
-			$TimeDiff = $Now - $LastHealthyCheck
-			Write-Host $URL.description
-			$Body = "<strong>URL:</strong><br><a href=""$URL"">$URL</a><br><br><strong>Response:</strong><br>$Status<br><br><strong>Last Healthy Check:</strong><br>$LastHealthyCheck"
-			Send-MailMessage -from $FromAddress -to $NotificationList -subject "URL Alert: $URL ($Description $Group)" -bodyashtml -body $Body -smtpServer $MailServer
-			Invoke-SQLCmd -Query "declare @failcount int; select @failcount = failcount + 1 from url where url = '$URL'; update URL set status = '$Status', lastcheck = getdate(), failcount = @failcount where url = '$URL'; insert into log (URL_ID, Response, CreatedOn, FailCount) values ('$ID', '$Status', getdate(), @failcount)" -ServerInstance $Instance -Database $Database
+			If ($FailCount % 2 -eq 1)
+			{
+				$Status = $_.Exception
+				$Status = $Status -replace "'",""
+				$Now = Get-Date
+				$TimeDiff = $Now - $LastHealthyCheck
+
+				$Body = "<strong>URL:</strong><br><a href=""$URL"">$URL</a><br><br><strong>Response:</strong><br>$Status<br><br><strong>Last Healthy Check:</strong><br>$LastHealthyCheck"
+				Send-MailMessage -from $FromAddress -to $NotificationList -subject "URL Alert: $URL ($Description $Group)" -bodyashtml -body $Body -smtpServer $MailServer
+				Invoke-SQLCmd -Query "declare @failcount int; select @failcount = failcount + 1 from url where url = '$URL'; update URL set status = '$Status', lastcheck = getdate(), failcount = @failcount where url = '$URL'; insert into log (URL_ID, Response, CreatedOn, FailCount) values ('$ID', '$Status', getdate(), @failcount)" -ServerInstance $Instance -Database $Database
+			}
 		}
 	}
 }
